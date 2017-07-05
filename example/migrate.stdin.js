c130be4fc5205955db6e3f0c035bba8c0c67ffee
Even Stensberg <evenstensberg@gmail.com>
2017-07-05 14:12:04 +0200

    fix runnerQueues

--- example/migrate.stdin.js
+++ example/migrate.stdin.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 /* global describe question */
 
 describe('migrate', () => {
-	question('Are you sure these changes are fine? (Y/n)', 'Y', (answer) => {
-		assert.equal(answer, 'Y');
-	});
+  question('Are you sure these changes are fine? (Y/n)', 'Y', (answer) => {
+    assert.equal(answer, 'Y');
+  });
 });
